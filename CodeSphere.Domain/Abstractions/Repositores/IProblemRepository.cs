c1c17d86e10eca62a34270c14a9a91aedfda17f7
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-11-01 06:28:14 +0200

    Fixed Get TestCases by problem id

---
+++ CodeSphere.Domain/Abstractions/Repositores/IProblemRepository.cs
@@ -0,0 +1,14 @@
+﻿using CodeSphere.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSphere.Domain.Abstractions.Repositores
+{
+    public interface IProblemRepository
+    {
+        IEnumerable<Testcase> GetTestCasesByProblemId(Guid problemId);
+    }
+}
