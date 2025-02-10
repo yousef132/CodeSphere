3297ee0b2fa7e2845ad315e53ed45d3db822a684
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-02-10 20:56:48 +0200

    fix: add some query parameters in GetAllProblemQuery

--- CodeSphere.Application/Features/Problem/Queries/GetAll/GetAllProblemsQuery.cs
+++ CodeSphere.Application/Features/Problem/Queries/GetAll/GetAllProblemsQuery.cs
@@ -12,8 +12,25 @@ namespace CodeSphere.Application.Features.Problem.Queries.GetAll
         public Difficulty? Difficulty { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
+        
+        
+        // status : AC , Attempted, Not Attempted
+         public string? Status { get; set; }
 
-        public GetAllProblemsQuery(string? userId, List<string>? topicsNames, string? problemName, Difficulty? difficulty, int pageNumber, int pageSize)
+        // sortBy : Name, Difficulty, acceptanceRate, 
+        public string? SortBy { get; set; }
+
+        // order : asc, desc
+        public string? Order { get; set; }
+        public GetAllProblemsQuery(string? userId,
+            List<string>? topicsNames,
+            string? problemName,
+            Difficulty? difficulty,
+            int pageNumber,
+            int pageSize,
+            string status,
+            string sortBy,
+            string? order)
         {
             // UserId = userId;
             TopicsNames = topicsNames;
@@ -21,6 +38,9 @@ namespace CodeSphere.Application.Features.Problem.Queries.GetAll
             Difficulty = difficulty;
             PageNumber = pageNumber;
             PageSize = pageSize;
+            Status = status;
+            SortBy = sortBy;
+            Order = order;
         }
     }
 
